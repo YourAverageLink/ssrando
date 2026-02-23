968b31829640b3e846d6af7eefd58171c7ae2f66
Xander D <youraveragelink@gmail.com>
2026-02-22 22:19:31 -0800

    oops fix asm

--- asm/custom-functions/src/lib.rs
+++ asm/custom-functions/src/lib.rs
@@ -83,14 +83,10 @@ fn custom_main_additions() -> u32 {
             }
         }
 
+        rando::give_ap_rs();
         if SHOULD_PRINT_AP_BUFFER {
             return crate::rando::print_archipelago_text();
         }
-    }
-
-    rando::give_ap_rs();
-    if unsafe { SHOULD_PRINT_AP_BUFFER } {
-        return crate::rando::print_archipelago_text();
     }
 
     return 1;
